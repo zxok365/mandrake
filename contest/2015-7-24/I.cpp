4b685a8b4e39a0a84be7d12b5f8d4b5786da9b56
Nerer <xunayun1996@163.com>
2015-07-24 20:11:38 +0800

    I.cpp retabbed.

--- contest/2015-7-24/I.cpp
+++ contest/2015-7-24/I.cpp
@@ -1,13 +1,16 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<algorithm>
-#include<cmath>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+
 using namespace std;
-int gcd(int a, int b){
-	return b ? gcd(b, a%b) : a;
+
+int gcd(int a, int b) {
+	return b ? gcd(b, a % b) : a;
 }
-void solve(){
+
+void solve() {
 	int x1, x2, x3, y1, y2, y3;
 	scanf("%d%d%d%d%d%d", &x1, &y1, &x2, &y2, &x3, &y3);
 	int ans = x1 * y2 - x2 * y1;
@@ -15,11 +18,11 @@ void solve(){
 	ans = gcd(ans, x1 * y3 - x3 * y1);
 	printf("%d\n", abs(ans));
 }
-int main(){
+
+int main() {
 	int tests;
 	scanf("%d", &tests);
-	for (int i = 1; i <= tests; i++)
-	{
+	for (int i = 1; i <= tests; i++) {
 		printf("Case #%d: ", i);
 		solve();
 	}
