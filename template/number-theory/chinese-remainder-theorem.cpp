6637929796d1a60b4ceb6ee0e892d4a071d6a086
dilutedream <zhijianliu.cs@gmail.com>
2015-07-24 11:30:01 +0800

    Chinese Remainder Theorem Added.

---
+++ template/number-theory/chinese-remainder-theorem.cpp
@@ -0,0 +1,21 @@
+namespace chinese_remainder_theorem {
+
+bool solve(int n, long long r[], long long m[], long long &remainder, long long &modular) {
+	remainder = modular = 1;
+	for (int i = 0; i < n; ++i) {
+		long long x, y;
+		euclid(modular, m[i], x, y);
+		long long divisor = gcd(modular, m[i]);
+		if ((r[i] - remainder) % divisor) {
+			return false;
+		}
+		x *= (r[i] - remainder) / divisor;
+		((x %= m[i]) += m[i]) %= m[i];
+		remainder += modular * x;
+		modular *= m[i] / divisor;
+		((remainder %= modular) += modular) %= modular;
+	}
+	return true;
+}
+
+}
