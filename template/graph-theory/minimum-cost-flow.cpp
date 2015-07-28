3b924b875093445e3e2945d49a12be02756d0e07
dilutedream <zhijianliu.cs@gmail.com>
2015-07-28 10:19:06 +0800

    Minimum Cost Flow added.

---
+++ template/graph-theory/minimum-cost-flow.cpp
@@ -0,0 +1,81 @@
+namespace minimum_cost_flow {
+
+const N = , M = ;
+
+struct EdgeList {
+	int size;
+	int last[N];
+	int succ[M], other[M], flow[M], cost[M];
+	
+	void clear(int n) {
+		size = 1;
+		fill(last + 1, last + n + 1, 0);
+	}
+	
+	void add(int x, int y, int c, int w) {
+		size++;
+		succ[size] = last[x];
+		last[x] = size;
+		other[size] = y;
+		flow[size] = c;
+		cost[size] = w;
+	}
+} e;
+
+int n, m, source, target;
+int prev[N];
+
+void add(int x, int y, int c, int w) {
+	e.add(x, y, c, w);
+	e.add(y, x, 0, -w);
+}
+
+bool spfa() {
+	static int dist[N];
+	static bool exist[N];
+	vector<int> queue;
+	fill(dist + 1, dist + n + 1, INT_MAX);
+	fill(exist + 1, exist + n + 1, 0);
+	queue.push_back(source);
+	dist[source] = 0;
+	exist[source] = true;
+	for (int head = 0; head < (int)queue.size(); ++head) {
+		int x = queue[head];
+		exist[x] = false;
+		for (int i = e.last[x]; i; i = e.succ[i]) {
+			int y = e.other[i];
+			if (e.flow[i] && dist[y] > dist[x] + e.cost[i]) {
+				dist[y] = dist[x] + e.cost[i];
+				prev[y] = i;
+				if (!exist[y]) {
+					exist[y] = true;
+					queue.push_back(y);
+				}
+			}
+		}
+	}
+	return dist[target] < INT_MAX;
+}
+
+pair<int, int> solve() {
+	int flow = 0, cost = 0;
+	for (; spfa(); ) {
+		int num = INT_MAX;
+		for (int i = target; i != source; i = e.other[prev[i] ^ 1]) {
+			num = min(num, e.flow[prev[i]]);
+		}
+		flow += num;
+		for (int i = target; i != source; i = e.other[prev[i] ^ 1]) {
+			e.flow[prev[i]] -= num;
+			e.flow[prev[i] ^ 1] += num;
+			cost += num * e.cost[prev[i]];
+		}
+	}
+	return make_pair(flow, cost);
+}
+
+void clear() {
+	e.clear(n);
+}
+
+}
