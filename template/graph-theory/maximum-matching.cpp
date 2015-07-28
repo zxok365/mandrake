1c7d4556e3d5f9c20242bc7de9de62ae13f11586
dilutedream <zhijianliu.cs@gmail.com>
2015-07-28 10:11:07 +0800

    Maximum Matching added.

---
+++ template/graph-theory/maximum-matching.cpp
@@ -0,0 +1,60 @@
+namespace maximum_matching {
+
+const int N = ;
+
+int n, m, times;
+vector<int> adj[N];
+int matchx[N], matchy[N];
+int visit[N];
+
+void add(int x, int y) {
+	adj[x].push_back(y);
+}
+
+bool dfs(int x) {
+	for (int i = 0; i < (int)adj[x].size(); ++i) {
+		int y = adj[x][i];
+		if (visit[y] != times) {
+			visit[y] = times;
+			if (!matchy[y] || dfs(matchy[y])) {
+				matchy[y] = x;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+int solve() {
+	fill(matchy + 1, matchy + m + 1, 0);
+	int ans = 0;
+	for (int i = 1; i <= n; ++i) {
+		times++;
+		ans += dfs(i);
+	}
+	for (int i = 1; i <= m; ++i) {
+		matchx[matchy[i]] = i;
+	}
+	return ans;
+}
+
+void greedy() {
+	fill(matchy + 1, matchy + m + 1, 0);
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 0; j < (int)adj[i].size(); ++j) {
+			int x = adj[i][j];
+			if (!matchy[x]) {
+				matchy[x] = i;
+				break;
+			}
+		}
+	}
+}
+
+void clear() {
+	for (int i = 1; i <= n; ++i) {
+		adj[i].clear();
+	}
+}
+
+}
