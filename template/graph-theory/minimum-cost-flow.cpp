d576fcb948da4a9760b3fc7e3a7bc6645b1c855b
Zhijian Liu <dilutedream@gmail.com>
2015-08-08 01:07:21 +0800

    Template modified.

--- template/graph-theory/minimum-cost-flow.cpp
+++ template/graph-theory/minimum-cost-flow.cpp
@@ -4,21 +4,20 @@ struct EdgeList {
 	int succ[M], other[M], flow[M], cost[M];
 	
 	void clear(int n) {
-		size = 1;
-		std::fill(last + 1, last + n + 1, 0);
+		size = 0;
+		std::fill(last, last + n, -1);
 	}
 	
 	void add(int x, int y, int c, int w) {
-		size++;
 		succ[size] = last[x];
 		last[x] = size;
 		other[size] = y;
 		flow[size] = c;
-		cost[size] = w;
+		cost[size++] = w;
 	}
 } e;
 
-int n, m, source, target;
+int n, source, target;
 int prev[N];
 
 void add(int x, int y, int c, int w) {
@@ -26,46 +25,45 @@ void add(int x, int y, int c, int w) {
 	e.add(y, x, 0, -w);
 }
 
-bool spfa() {
-	static int dist[N];
-	static bool exist[N];
+bool augment() {
+	static int dist[N], occur[N];
 	std::vector<int> queue;
-	std::fill(dist + 1, dist + n + 1, INT_MAX);
-	std::fill(exist + 1, exist + n + 1, 0);
-	queue.push_back(source);
+	std::fill(dist, dist + n, INT_MAX);
+	std::fill(occur, occur + n, 0);
 	dist[source] = 0;
-	exist[source] = true;
+	occur[source] = true;
+	queue.push_back(source);
 	for (int head = 0; head < (int)queue.size(); ++head) {
 		int x = queue[head];
-		exist[x] = false;
-		for (int i = e.last[x]; i; i = e.succ[i]) {
+		for (int i = e.last[x]; ~i; i = e.succ[i]) {
 			int y = e.other[i];
 			if (e.flow[i] && dist[y] > dist[x] + e.cost[i]) {
 				dist[y] = dist[x] + e.cost[i];
 				prev[y] = i;
-				if (!exist[y]) {
-					exist[y] = true;
+				if (!occur[y]) {
+					occur[y] = true;
 					queue.push_back(y);
 				}
 			}
 		}
+		occur[x] = false;
 	}
 	return dist[target] < INT_MAX;
 }
 
 std::pair<int, int> solve() {
-	int flow = 0, cost = 0;
-	for (; spfa(); ) {
-		int num = INT_MAX;
+	std::pair<int, int> answer = std::make_pair(0, 0);
+	while (augment()) {
+		int number = INT_MAX;
 		for (int i = target; i != source; i = e.other[prev[i] ^ 1]) {
-			num = min(num, e.flow[prev[i]]);
+			number = std::min(number, e.flow[prev[i]]);
 		}
-		flow += num;
+		answer.first += number;
 		for (int i = target; i != source; i = e.other[prev[i] ^ 1]) {
-			e.flow[prev[i]] -= num;
-			e.flow[prev[i] ^ 1] += num;
-			cost += num * e.cost[prev[i]];
+			e.flow[prev[i]] -= number;
+			e.flow[prev[i] ^ 1] += number;
+			answer.second += number * e.cost[prev[i]];
 		}
 	}
-	return std::make_pair(flow, cost);
+	return answer;
 }
