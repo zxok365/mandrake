fe04bdc34d739d965e2051b40534ba5109df0511
Zhijian Liu <dilutedream@gmail.com>
2015-08-07 23:34:58 +0800

    Variables renamed.

--- template/data-structure/range-minimum-query.cpp
+++ template/data-structure/range-minimum-query.cpp
@@ -1,23 +1,21 @@
-const int N = , M = ;
-
 int log2[N];
-pair<int, int> num[N][M];
+pair<int, int> value[N][20];
 
-void build(int n, int a[]) {
-	for (int i = 2; i <= n; ++i) {
+void build(int length, int number[]) {
+	for (int i = 2; i <= length; ++i) {
 		log2[i] = log2[i >> 1] + 1;
 	}
-	for (int i = 1; i <= n; ++i) {
-		num[i][0] = make_pair(a[i], i);
+	for (int i = 1; i <= length; ++i) {
+		value[i][0] = make_pair(number[i], i);
 	}
-	for (int j = 1; (1 << j) <= n; ++j) {
-		for (int i = 1; i + (1 << j) - 1 <= n; ++i) {
-			num[i][j] = min(num[i][j - 1], num[i + (1 << j - 1)][j - 1]);
+	for (int step = 1; (1 << step) <= length; ++step) {
+		for (int i = 1; i + (1 << step) - 1 <= length; ++i) {
+			value[i][step] = min(value[i][step - 1], value[i + (1 << step - 1)][step - 1]);
 		}
 	}
 }
 
-pair<int, int> solve(int left, int right) {
-	int k = log2[right - left + 1];
-	return min(num[left][k], num[right - (1 << k) + 1][k]);
+pair<int, int> get(int left, int right) {
+	int step = log2[right - left + 1];
+	return min(value[left][step], value[right - (1 << step) + 1][step]);
 }
