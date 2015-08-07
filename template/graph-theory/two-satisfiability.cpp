7ac5e1c6cec93244cf351c8cd4b49ca3d889b61d
Zhijian Liu <dilutedream@gmail.com>
2015-08-08 00:59:54 +0800

    Template modified.

--- template/graph-theory/two-satisfiability.cpp
+++ template/graph-theory/two-satisfiability.cpp
@@ -1,23 +1,20 @@
-const int N = ;
-
-int n, times, comps, top;
-vector<int> adj[N];
+int stamp, comps, top;
 int dfn[N], low[N], comp[N], stack[N];
 
 void add(int x, int a, int y, int b) {
-	adj[x << 1 | a].push_back(y << 1 | b);
+	edge[x << 1 | a].push_back(y << 1 | b);
 }
 
 void tarjan(int x) {
-	dfn[x] = low[x] = ++times;
+	dfn[x] = low[x] = ++stamp;
 	stack[top++] = x;
-	for (int i = 0; i < (int)adj[x].size(); ++i) {
-		int y = adj[x][i];
+	for (int i = 0; i < (int)edge[x].size(); ++i) {
+		int y = edge[x][i];
 		if (!dfn[y]) {
 			tarjan(y);
-			low[x] = min(low[x], low[y]);
+			low[x] = std::min(low[x], low[y]);
 		} else if (!comp[y]) {
-			low[x] = min(low[x], dfn[y]);
+			low[x] = std::min(low[x], dfn[y]);
 		}
 	}
 	if (low[x] == dfn[x]) {
@@ -30,25 +27,20 @@ void tarjan(int x) {
 }
 
 bool solve() {
-	times = top = comps = 0;
-	fill(dfn + 1, dfn + (n << 1 | 1) + 1, 0);
-	fill(comp + 1, comp + (n << 1 | 1) + 1, 0);
-	for (int i = 2; i <= (n << 1 | 1); ++i) {
+	int counter = n + n + 1;
+	stamp = top = comps = 0;
+	std::fill(dfn, dfn + counter, 0);
+	std::fill(comp, comp + counter, 0);
+	for (int i = 0; i < counter; ++i) {
 		if (!dfn[i]) {
 			tarjan(i);
 		}
 	}
-	for (int i = 1; i <= n; ++i) {
+	for (int i = 0; i < n; ++i) {
 		if (comp[i << 1] == comp[i << 1 | 1]) {
 			return false;
 		}
-		ans[i] = (comp[i << 1 | 1] < comp[i << 1]);
+		answer[i] = (comp[i << 1 | 1] < comp[i << 1]);
 	}
 	return true;
-}
-
-void clear() {
-	for (int i = 2; i <= (n << 1 | 1); ++i) {
-		adj[i].clear();
-	}
 }
