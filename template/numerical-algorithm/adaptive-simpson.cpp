068b579d199fca912ef816cc5247f14bd6ccff58
Zhijian Liu <dilutedream@gmail.com>
2015-08-08 02:01:24 +0800

    Template modified.

--- template/numerical-algorithm/adaptive-simpson.cpp
+++ template/numerical-algorithm/adaptive-simpson.cpp
@@ -1,22 +1,19 @@
-template<typename function>
-double area(function f, const double &left, const double &right) {
+double area(const double &left, const double &right) {
 	double mid = (left + right) / 2;
-	return (right - left) * (f(left) + 4 * f(mid) + f(right)) / 6;
+	return (right - left) * (calc(left) + 4 * calc(mid) + calc(right)) / 6;
 }
 
-template<typename function>
-double simpson(function f, const double &left, const double &right, const double &eps, const double &area_sum) {
+double simpson(const double &left, const double &right, const double &eps, const double &area_sum) {
 	double mid = (left + right) / 2;
-	double area_left = area(f, left, mid);
-	double area_right = area(f, mid, right);
+	double area_left = area(left, mid);
+	double area_right = area(mid, right);
 	double area_total = area_left + area_right;
-	if (fabs(area_total - area_sum) <= 15 * eps) {
+	if (std::abs(area_total - area_sum) < 15 * eps) {
 		return area_total + (area_total - area_sum) / 15;
 	}
-	return simpson(f, left, right, eps / 2, area_left) + simpson(f, mid, right, eps / 2, area_right);
+	return simpson(left, right, eps / 2, area_left) + simpson(mid, right, eps / 2, area_right);
 }
 
-template<typename function>
-double simpson(function f, const double &left, const double &right, const double &eps) {
-	return simpson(f, left, right, eps, area(f, left, right));
+double simpson(const double &left, const double &right, const double &eps) {
+	return simpson(left, right, eps, area(left, right));
 }
