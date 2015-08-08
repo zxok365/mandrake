3a4b3fb60e141e9e17147462d17e3d12ba281f5c
dilutedream <zhijianliu.cs@gmail.com>
2015-08-08 18:29:29 +0800

    Modify text to pattern.

--- template/string-manipulation/knuth-morris-pratt.cpp
+++ template/string-manipulation/knuth-morris-pratt.cpp
@@ -1,8 +1,8 @@
-void build(char *text) {
-	int length = (int)strlen(text + 1);
+void build(char *pattern) {
+	int length = (int)strlen(pattern + 1);
 	fail[0] = -1;
 	for (int i = 1, j; i <= length; ++i) {
-		for (j = fail[i - 1]; j != -1 && text[i] != text[j + 1]; j = fail[j]);
+		for (j = fail[i - 1]; j != -1 && pattern[i] != pattern[j + 1]; j = fail[j]);
 		fail[i] = j + 1;
 	}
 }
