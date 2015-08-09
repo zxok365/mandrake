1f5ed5659bb9fec0cf5475173cb58a0336e7fb34
dilutedream <zhijianliu.cs@gmail.com>
2015-08-09 18:41:00 +0800

    Suffix Automation modified.

--- template/string-manipulation/suffix-automation.cpp
+++ template/string-manipulation/suffix-automation.cpp
@@ -1,26 +1,26 @@
-class State {
+class Node {
 public:
-	State *child[N], *parent;
+	Node *child[N], *parent;
 	int length;
 	
-	State(int length) : parent(NULL), length(length) {
+	Node(int length) : parent(NULL), length(length) {
 		memset(child, NULL, sizeof(child));
 	}
 	
-	State* extend(State *start, int token) {
-		State *p = this;
-		State *np = new State(length + 1);
+	Node* extend(Node *start, int token) {
+		Node *p = this;
+		Node *np = new Node(length + 1);
 		for (; p && !p->child[token]; p = p->parent) {
 			p->child[token] = np;
 		}
 		if (!p) {
 			np->parent = start;
 		} else {
-			State *q = p->child[token];
+			Node *q = p->child[token];
 			if (p->length + 1 == q->length) {
 				np->parent = q;
 			} else {
-				State *nq = new State(p->length + 1);
+				Node *nq = new Node(p->length + 1);
 				memcpy(nq->child, q->child, sizeof(q->child));
 				nq->parent = q->parent;
 				np->parent = q->parent = nq;
