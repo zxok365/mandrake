4b3bada2ce7c15a167a5b6bd7e5dde799bc39169
Zhijian Liu <dilutedream@gmail.com>
2015-09-05 17:02:08 +0800

    Persistent Treap updated.

--- template/source/data-structure/persistent-treap.cpp
+++ template/source/data-structure/persistent-treap.cpp
@@ -1,15 +1,15 @@
 class Node {
+public:
     Node *left, *right;
     int size;
 
-    Node(Node *left, Node *right) : left(left), right(right) {}
+    Node();
+    std::pair<Node*, Node*> split(int size);
 
     Node* update() {
         size = left->size + 1 + right->size;
         return this;
     }
-
-    Pair split(int size);
 };
 
 bool random(int a, int b) {
@@ -17,6 +17,8 @@ bool random(int a, int b) {
 }
 
 Node *null;
+
+Node::Node() : left(null), right(null), size(1) {}
 
 Node* merge(Node *x, Node *y) {
     if (x == null) {
@@ -43,13 +45,13 @@ std::pair<Node*, Node*> Node::split(int size) {
         left = null;
         return std::make_pair(result.first, merge(result.second, this->update()));
     } else {
-        std::pair<Node*, Node*> result = right->split(size - left->size);
+        std::pair<Node*, Node*> result = right->split(size - left->size - 1);
         right = null;
         return std::make_pair(merge(this->update(), result.first), result.second);
     }
 }
 
 void build() {
-    null = new Node(NULL, NULL);
-    null->left = null->right = null;
+    null = new Node();
+    null->size = 0;
 }
